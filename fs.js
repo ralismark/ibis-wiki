4798ab983985d6396720bf875e4283e58079b38f
ralismark <13449732+ralismark@users.noreply.github.com>
2020-12-18 11:38:27 +1100

    Move mode definition

--- fs.js
+++ fs.js
@@ -1,56 +1,5 @@
 "use strict";
 
-CodeMirror.defineMode("mdm", () => ({
-  startState: () => ({ state: "normal" }),
-
-  copyState: ({ state }) => { state },
-
-  token: (stream, state) => {
-
-    if(state.state == "skiptoend") {
-      stream.skipToEnd();
-      state.state = "normal";
-      return null;
-    }
-
-    if(stream.sol()) {
-      // forbid multi-line formatting
-      state.state = "normal";
-
-      let heading = stream.match(/#{1,6}/);
-      if(heading) return `line-h${heading.length} line-h meta`;
-
-    }
-
-    if(state.state == "normal") {
-
-      if(stream.match("]]")) {
-        return "bracket";
-      }
-
-      if(stream.match("[[")) {
-        state.state = "link";
-        return "bracket";
-      }
-
-      stream.next();
-      return null;
-
-    } else if(state.state == "link") {
-
-      if(stream.skipTo("]]")) {
-        state.state = "normal";
-        return "link local-link";
-      }
-
-      stream.skipToEnd();
-      state.state = "normal";
-      return null;
-
-    }
-  },
-}));
-
 window.REPO = (() => {
 
   const fs = new LightningFS("fs");
