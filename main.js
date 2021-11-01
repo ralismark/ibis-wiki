05f0e69955e793aad55967c5a8d516960a8e8681
ralismark <13449732+ralismark@users.noreply.github.com>
2021-11-02 00:14:35 +1100

    Revamp markdown handling

--- main.js
+++ main.js
@@ -12,9 +12,8 @@ import {closeBrackets, closeBracketsKeymap} from "@codemirror/closebrackets"
 import {defaultHighlightStyle} from "@codemirror/highlight"
 
 // our own parser
-import {parser} from "./mdm.grammar"
-import {LezerLanguage, LanguageSupport, indentService} from "@codemirror/language"
-import {styleTags, tags as t, Tag} from "@codemirror/highlight"
+import * as md from "@lezer/markdown"
+import {LRLanguage, LanguageSupport, indentService} from "@codemirror/language"
 
 import {stringInput} from "lezer-tree"
 
@@ -82,70 +81,19 @@ function printTree(
   }
 }
 
-function longParse(str) {
+function longParse(parser, str) {
   const parsed = parser.parse(str);
   return printTree(parsed, str);
-}
-
-const tx = {
-  metaFor: Tag.defineModifier(),
-  withStrong: Tag.defineModifier(),
-  withEm: Tag.defineModifier(),
-  refLink: Tag.define(t.link),
-  urlLink: Tag.define(t.link),
-};
-
-const lang = LezerLanguage.define({
-  parser: parser.configure({
-    props: [
-      styleTags({
-        Preamble: t.monospace,
-
-        Header: t.heading,
-        ATXMeta: tx.metaFor(t.heading),
-
-        Blockquote: t.quote,
-        BlockquoteMeta: tx.metaFor(t.quote),
-
-        Codeblock: t.monospace,
-        CodeblockFence: tx.metaFor(t.monospace),
-        CodeSpan: t.monospace,
-        CodeSpanMeta: tx.metaFor(t.monospace),
-
-        S1: tx.withEm(t.content),
-        S2: tx.withStrong(t.content),
-        S3: tx.withStrong(tx.withEm(t.content)),
-
-        Star: t.punctuation,
-        Starstar: t.punctuation,
-
-        RefOpen: t.squareBracket,
-        RefClose: t.squareBracket,
-        RefIdent: tx.refLink,
-
-        AngOpen: t.angleBracket,
-        AngClose: t.angleBracket,
-        AngIdent: tx.urlLink,
-      }),
-    ],
-  }),
-
-  languageData: {
-  },
-});
-
-function filetype() {
-  return new LanguageSupport(lang);
 }
 
 window.CM = {
   state,
   view,
   commands,
-  filetype,
+  md,
+  makeLang: x => new LanguageSupport(LRLanguage.define(x)),
   longParse,
   highlight,
-  tx,
   basicSetup: [
     highlightSpecialChars(),
     history(),
