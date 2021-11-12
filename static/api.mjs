ef1a279dd6ae792cf59459b7daaa0a1f0e623644
ralismark <13449732+ralismark@users.noreply.github.com>
2021-11-12 13:04:18 +1100

    Use WebDAV for api

--- static/api.mjs
+++ static/api.mjs
@@ -25,8 +25,39 @@ function xhr(prepare) {
 
 export async function list() {
   const { content, raise_for_status } = await xhr(req => {
-    req.responseType = "json";
+    req.open("PROPFIND", Config.API_BASE);
+    req.responseType = "document";
+    req.setRequestHeader("Depth", "1");
+    req.send(`
+<?xml version="1.0" encoding="utf-8"?>
+<propfind xmlns="DAV:">
+  <prop>
+    <resourcetype xmlns="DAV:"/>
+  </prop>
+</propfind>
+    `);
+  });
+  raise_for_status();
+
+  const entries = [];
+
+  for(let resp of content.documentElement.children) {
+    const isCollection = resp.querySelector("propstat > prop > resourcetype > collection") !== null;
+    if(isCollection) continue;
+
+    const path = resp.querySelector("href").textContent;
+    const filename = path.split("/").pop();
+
+    entries.push(filename);
+  }
+
+  return entries;
+}
+
+export async function old_list() {
+  const { content, raise_for_status } = await xhr(req => {
     req.open("GET", Config.API_BASE + "list");
+    req.responseType = "json";
     req.send();
   });
   raise_for_status();
@@ -35,23 +66,44 @@ export async function list() {
 
 export async function load(path) {
   let { content, status, header, raise_for_status } = await xhr(req => {
+    req.open("GET", Config.API_BASE + path);
     req.responseType = "text"
-    req.open("GET", Config.API_BASE + "data/" + path);
     req.send();
   });
 
-  // TODO 2021-11-11 clean up this logic
-
   // we "gracefully" degrade to not validating ETag
-  let token = "*";
+  let token = header.ETag || "*";
 
   // special handling of 404
   if(status === 404) {
     raise_for_status = () => {}; // don't throw
     content = "";
     token = null;
-  } else if(Config.ETAGS && header.ETag) {
-    token = header.ETag;
+  }
+
+  raise_for_status();
+
+  console.log("[api]", path, "loaded.", "etag:", token);
+  return {token, content};
+}
+
+export async function old_load(path) {
+  let { content, status, header, raise_for_status } = await xhr(req => {
+    req.open("GET", Config.API_BASE + "data/" + path);
+    req.responseType = "text"
+    req.send();
+  });
+
+  // TODO 2021-11-11 clean up this logic
+
+  // we "gracefully" degrade to not validating ETag
+  let token = header.ETag || "*";
+
+  // special handling of 404
+  if(status === 404) {
+    raise_for_status = () => {}; // don't throw
+    content = "";
+    token = null;
   }
 
   raise_for_status();
@@ -61,6 +113,34 @@ export async function load(path) {
 }
 
 export async function store(path, content, token) {
+  if(Config.READONLY) return;
+
+  const isDelete = content === "";
+
+  const { header, raise_for_status } = await xhr(req => {
+    req.open(isDelete ? "DELETE" : "PUT", Config.API_BASE + path);
+
+    // token verif
+    if(Config.ETAGS) {
+      if(token === null)
+        req.setRequestHeader("If-None-Match", "*");
+      else
+        req.setRequestHeader("If-Match", token);
+    }
+
+    req.send(content);
+  });
+
+  raise_for_status();
+
+  // we "gracefully" degrade to not validating ETag
+  token = isDelete ? null : header.ETag || "*";
+
+  console.log("[api]", path, "stored.", "etag:", token);
+  return token;
+}
+
+export async function old_store(path, content, token) {
   if(Config.READONLY) return;
   const { header, raise_for_status } = await xhr(req => {
     req.open("PUT", Config.API_BASE + "data/" + path);
