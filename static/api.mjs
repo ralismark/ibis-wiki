22bc16d448eac8cbdde1f0877928f0950a9cc241
ralismark <13449732+ralismark@users.noreply.github.com>
2022-04-16 15:28:32 +1000

    Backend refactor

--- static/api.mjs
+++ static/api.mjs
@@ -27,7 +27,7 @@ function xhr(prepare) {
   });
 }
 
-const webdav_api = {
+class WebdavStorage {
   async list() {
     const { status, content, raise_for_status } = await xhr(req => {
       req.open("PROPFIND", schema.WEBDAV_URL.get());
@@ -58,7 +58,7 @@ const webdav_api = {
     }
 
     return entries;
-  },
+  }
 
   async load(path) {
     let { content, status, header, raise_for_status } = await xhr(req => {
@@ -81,7 +81,7 @@ const webdav_api = {
       etag: header.ETag || "*", // we "gracefully" degrade to not validating ETag
       content,
     };
-  },
+  }
 
   async delete(path, etag) {
     const { header, raise_for_status } = await xhr(req => {
@@ -95,7 +95,7 @@ const webdav_api = {
       req.send();
     });
     raise_for_status();
-  },
+  }
 
   async store_nonempty(path, content, etag) {
     const { header, raise_for_status } = await xhr(req => {
@@ -113,10 +113,73 @@ const webdav_api = {
     return {
       etag: header.ETag || "*", // we "gracefully" degrade to not validating ETag
     }
-  },
+  }
 };
 
-const backend = webdav_api; // TODO s3 support
+class NoneStorage {
+  constructor() {
+    // in-memory store of page contents
+    this.pages = {};
+  }
+
+  async list() {
+    // try the best we can
+    return Object.keys(this.pages);
+  }
+
+  async load(path) {
+    // try getting from local storage
+    const saved_content = this.pages[path];
+    if(saved_content !== undefined) {
+      return {etag: "*", content: saved_content};
+    }
+
+    let { content, status, raise_for_status } = await xhr(req => {
+      req.open("GET", schema.GET_URL.get() + path);
+      req.responseType = "text";
+
+      // https://stackoverflow.com/a/48969579
+      req.setRequestHeader("Cache-Control", "no-cache, no-store, max-age=0");
+      req.setRequestHeader("Expires", "Tue, 01 Jan 1980 1:00:00 GMT");
+      req.setRequestHeader("Pragma", "no-cache");
+
+      req.send();
+    });
+
+    // special handling of 404
+    if(status === 404) return {etag: null, content: ""};
+    raise_for_status();
+
+    return {etag: "*", content};
+  }
+
+  async delete(path, etag) {
+    // TODO maybe more rigorous checking?
+    delete this.pages[path];
+  }
+
+  async store_nonempty(path, content, etag) {
+    this.pages[path] = content;
+    return {etag: "*"};
+  }
+};
+
+const backend_mapping = {
+  "none": NoneStorage,
+  "webdav": WebdavStorage,
+};
+
+const backend = (() => {
+  const backend = backend_mapping[schema.STORAGE_TYPE.get()];
+  if(backend === undefined) {
+    reportError(
+      `"${schema.STORAGE_TYPE.get()}" is not a valid STORAGE_TYPE, must be one of `
+      + Object.keys(backend_mapping).map(x => `"${x}"`).join(", ")
+    );
+    return new NoneStorage;
+  }
+  return new backend;
+})();
 
 export async function list() {
   return await backend.list();
