b26aa064c220d113907f59b82335d2fa0fb09fa8
ralismark <13449732+ralismark@users.noreply.github.com>
2023-10-21 18:00:11 +1100

    Improve searchbar

--- src/components/IbisSearch.tsx
+++ src/components/IbisSearch.tsx
@@ -1,25 +1,57 @@
-import { useEffect, useMemo, useState } from "react"
+import { useEffect, useId, useMemo, useRef, useState } from "react"
 import "./IbisSearch.css"
 import { FacadeExtern } from "../backend"
 import { IbisController } from "../App"
-import { useExtern, useExternOr } from "../extern"
+import { useExtern } from "../extern"
 
-type Suggestion = [string, JSX.Element, () => void]
+type Suggestion = {
+  key: string
+  content: JSX.Element,
+  fn: () => void,
+}
+
+// only using this to scroll the option into view when selected
+function Option({ suggestion, selected, ...props }: {
+  suggestion: Suggestion,
+  selected: boolean,
+} & React.LiHTMLAttributes<HTMLLIElement>) {
+  const ref = useRef<HTMLLIElement | null>(null)
+
+  useEffect(() => {
+    if (selected) {
+      ref.current?.scrollIntoView({
+        block: "nearest",
+      })
+    }
+  }, [ref, selected])
+
+  return <li
+    ref={ref}
+    {...props}
+  >{suggestion.content}</li>
+}
 
 export function IbisSearch() {
   const controller = useExtern(IbisController)
   const facade = useExtern(FacadeExtern)
   const [queryOrig, setQuery] = useState("")
   const query = queryOrig.trim()
+  const [selected, setSelected] = useState("")
 
-  const renderSug = ([key, content, cb]: Suggestion) => <li
-    key={key}
-    onClick={e => {
-      e.preventDefault()
-      cb()
-      setQuery("")
-    }}
-  >{content}</li>
+  // reset selection when search cleared
+  useEffect(() => {
+    if (query === "") setSelected("")
+  }, [query])
+
+  // Suggestions --------------------------------------------------------------
+
+  const topSugs: Suggestion[] = [
+    {
+      key: "",
+      content: <>Open card "{query}"</>,
+      fn: () => controller.open(query),
+    }
+  ]
 
   const listingSugs: Suggestion[] = useMemo(() => {
     // we don't use useExtern here since we don't wanna recalculate search
@@ -31,23 +63,68 @@ export function IbisSearch() {
     if (parts.length === 0) return []
     return Array.from(listing)
         .filter(path => parts.every(part => path.toLowerCase().includes(part)))
-        .map(path => [
-          "card-title/" + path,
-          <>{path}</>,
-          () => controller.open(path),
-        ])
+        .map(path => ({
+          key: "card-title/" + path,
+          content: <>{path}</>,
+          fn: () => controller.open(path),
+        }))
   }, [query])
 
   const [searchSugs, setSearchSugs] = useState<Suggestion[]>([])
   useEffect(() => {
     facade?.fts.search(query).then(results => {
-      setSearchSugs(results.map(path => [
-        "search/" + path,
-        <>{path}</>,
-        () => controller.open(path),
-      ]))
+      setSearchSugs(results.map(path => ({
+        key: "search/" + path,
+        content: <>{path}</>,
+        fn: () => controller.open(path),
+      })))
     })
   }, [facade, query])
+
+  // Functions ----------------------------------------------------------------
+
+  const getNextPrev = (): [string, string] => {
+    // TODO this could probably be optimised
+    const allSugs = [...topSugs, ...listingSugs, ...searchSugs]
+    const idx = allSugs.findIndex(({key}) => key === selected)
+    if (idx === -1) return ["", ""]
+    return [
+      allSugs[idx-1]?.key || allSugs[allSugs.length - 1]?.key || "",
+      allSugs[idx+1]?.key || "",
+    ]
+  }
+
+  const accept = () => {
+    for (const sugs of [topSugs, listingSugs, searchSugs]) {
+      const match = sugs.find(({key}) => key === selected)
+      if (match) {
+        match.fn()
+        setQuery("")
+        return
+      }
+    }
+  }
+
+  // UI -----------------------------------------------------------------------
+
+  const resultsId = useId()
+  const activeId = useId()
+
+  const renderSug = (s: Suggestion) => <Option
+    key={s.key}
+    suggestion={s}
+    selected={s.key === selected}
+
+    role="option"
+    onClick={e => {
+      e.preventDefault()
+      s.fn()
+      setQuery("")
+    }}
+    onMouseOver={() => setSelected(s.key)}
+    aria-selected={s.key === selected}
+    id={s.key === selected ? activeId : undefined}
+  />
 
   return <search className="ibis-search">
     <input
@@ -56,26 +133,47 @@ export function IbisSearch() {
       onChange={e => setQuery(e.target.value)}
       onKeyDown={e => {
         if (e.code === "Escape") setQuery("")
+        else if (e.code === "ArrowUp") setSelected(getNextPrev()[0])
+        else if (e.code === "ArrowDown") setSelected(getNextPrev()[1])
+        else if (e.code === "Enter") accept()
+        else console.log(e.code)
       }}
       autoComplete="off"
       placeholder="Search"
+      role="combobox"
+      aria-label="Search"
+      aria-expanded={query !== ""}
+      aria-controls={resultsId}
     />
-    {query && <div className="results">
-      <ul>
-        {renderSug(["", <>Open card "{query}"</>, () => controller.open(query)])}
-      </ul>
-      {listingSugs.length > 0 && <>
-        <p className="search-section">Title matches:</p>
-        <ul>
-          {listingSugs.map(renderSug)}
+    {query && <>
+      <div
+        className="results"
+        id={resultsId}
+        role="listbox"
+        aria-activedescendant={activeId}
+      >
+        <ul role="group">
+          {topSugs.map(renderSug)}
         </ul>
-      </>}
-      {searchSugs.length > 0 && <>
-        <p className="search-section">Content matches:</p>
-        <ul>
-          {searchSugs.map(renderSug)}
-        </ul>
-      </>}
-    </div>}
+        {listingSugs.length > 0 && <>
+          <p className="search-section">Title matches:</p>
+          <ul role="group">
+            {listingSugs.map(renderSug)}
+          </ul>
+        </>}
+        {searchSugs.length > 0 && <>
+          <p className="search-section">Content matches:</p>
+          <ul role="group">
+            {searchSugs.map(renderSug)}
+          </ul>
+        </>}
+      </div>
+
+      <div
+        className="absorb-input"
+        role="none"
+        onClick={() => setQuery("")}
+      />
+    </>}
   </search>
 }
