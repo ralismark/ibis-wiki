110632eaef6744f332b6c7d159aab72711b158ed
ralismark <13449732+ralismark@users.noreply.github.com>
2024-05-13 13:08:22 +1000

    PWA

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import checker from "vite-plugin-checker";
+import { VitePWA } from "vite-plugin-pwa";
 import postcssNesting from "postcss-nesting";
 import postcssImportUrl from "postcss-import-url";
 
@@ -11,6 +12,25 @@ export default defineConfig({
     react(),
     checker({
       typescript: true,
+    }),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: "Ibis Wiki",
+        short_name: "Ibis Wiki",
+        description: "Temmie's personal wiki project",
+        theme_color: "#402634",
+        icons: [
+          {
+            src: "./ibis-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
     }),
   ],
   css: {
@@ -24,12 +44,6 @@ export default defineConfig({
   },
   base: "",
   build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        static: resolve(__dirname, "static/index.html"),
-      },
-    },
     sourcemap: true,
   },
 });
