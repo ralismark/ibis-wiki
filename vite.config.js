a15f8a97e56899d33cee47da13790974751feab9
ralismark <13449732+ralismark@users.noreply.github.com>
2024-05-13 12:43:26 +1000

    Import foundation.css at build time

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import checker from "vite-plugin-checker";
+import postcssNesting from "postcss-nesting";
+import postcssImportUrl from "postcss-import-url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +13,15 @@ export default defineConfig({
       typescript: true,
     }),
   ],
+  css: {
+    postcss: {
+      plugins: [
+        postcssNesting,
+        postcssImportUrl({
+        }),
+      ],
+    },
+  },
   base: "",
   build: {
     rollupOptions: {
